d1edcbdcf75faa3d90ca33b6e4b14f36aff8402a
hungnguyen <hungnguyen@ankvn.com>
2024-11-17 20:49:09 +0700

    add found recipe page

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
   runApp(MaterialApp(
     title: "Fridge",
     debugShowCheckedModeBanner: false,
-    initialRoute: Routes.main,
+    initialRoute: Routes.foundRecipes,
     onGenerateRoute: (settings) => Routes.getRoutes(settings),
     theme: ThemeData(useMaterial3: true),
     // navigatorObservers: [],
