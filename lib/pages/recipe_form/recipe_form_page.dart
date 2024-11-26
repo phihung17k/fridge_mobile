35b6e2535fdeca5186858a82b8e716da7c11367d
Hung Nguyen <hungnguyen@ankvn.com>
2024-11-26 16:35:46 +0700

    update

--- lib/pages/recipe_form/recipe_form_page.dart
+++ lib/pages/recipe_form/recipe_form_page.dart
@@ -10,15 +10,43 @@ class RecipeFormPage extends StatefulWidget {
 class _RecipeFormPageState extends State<RecipeFormPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(
-          onPressed: () => Navigator.pop(context),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: BackButton(
+            onPressed: () => Navigator.pop(context),
+          ),
+          title: const Text("Selected Ingredient"),
+          forceMaterialTransparency: true,
+          scrolledUnderElevation: 2,
+          surfaceTintColor: Colors.amber,
+          bottom: TabBar(
+            indicatorSize: TabBarIndicatorSize.tab,
+            onTap: (value) {},
+            tabs: const <Widget>[
+              Tab(text: "Overview"),
+              Tab(text: "Ingredients"),
+              Tab(text: "Steps"),
+            ],
+          ),
         ),
-        title: const Text("Selected Ingredient"),
-        forceMaterialTransparency: true,
-        scrolledUnderElevation: 2,
-        surfaceTintColor: Colors.amber,
+        body: TabBarView(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(labelText: "Name"),
+                    validator: (value) {},
+                  )
+                ],
+              ),
+            ),
+            Text("2"),
+            Text("3"),
+          ],
+        ),
       ),
     );
   }
