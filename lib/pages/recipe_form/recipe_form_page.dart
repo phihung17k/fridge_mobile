3214d641403ea8542da984ebbe8868fb5a535a60
Hung Nguyen <hungnguyen@ankvn.com>
2024-12-03 10:34:13 +0700

    add fab in ingredient form

--- lib/pages/recipe_form/recipe_form_page.dart
+++ lib/pages/recipe_form/recipe_form_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fridge_mobile/pages/recipe_form/overview_form.dart';
 
@@ -78,47 +77,62 @@ class _RecipeFormPageState extends State<RecipeFormPage> {
           body: TabBarView(
             children: [
               const OverviewForm(),
-              ReorderableListView.builder(
-                // buildDefaultDragHandles: false,
-                padding: const EdgeInsets.all(8),
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return Card(
-                    key: Key("reorder item $index"),
-                    elevation: 2,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                    color: Colors.white,
-                    margin: const EdgeInsets.only(bottom: 8),
-                    child: ListTile(
-                      contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                      leading: const Image(
-                        image: AssetImage("assets/images/broccoli.png"),
-                      ),
-                      title: Text("Title $index"),
-                      trailing: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.close,
-                          color: Colors.red,
+              Scaffold(
+                body: ReorderableListView.builder(
+                  // buildDefaultDragHandles: false,
+                  padding: const EdgeInsets.all(8),
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      key: Key("reorder item $index"),
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      color: Colors.white,
+                      margin: const EdgeInsets.only(bottom: 8),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                        leading: const Image(
+                          image: AssetImage("assets/images/broccoli.png"),
+                        ),
+                        title: Text("Title $index"),
+                        trailing: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.close,
+                            color: Colors.red,
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
-                onReorder: (oldIndex, newIndex) {},
-                proxyDecorator: (child, index, animation) {
-                  return AnimatedBuilder(
-                    animation: animation,
-                    builder: (BuildContext context, Widget? child) {
-                      // animation's effect for reorder item
-                      return Material(
-                        color: Colors.transparent,
-                        child: child,
-                      );
-                    },
-                    child: child,
-                  );
-                },
+                    );
+                  },
+                  onReorder: (oldIndex, newIndex) {},
+                  proxyDecorator: (child, index, animation) {
+                    return AnimatedBuilder(
+                      animation: animation,
+                      builder: (BuildContext context, Widget? child) {
+                        // animation's effect for reorder itemw
+                        return Material(
+                          color: Colors.transparent,
+                          child: child,
+                        );
+                      },
+                      child: child,
+                    );
+                  },
+                ),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return Container(
+                          color: Colors.amber,
+                        );
+                      },
+                    );
+                  },
+                  child: const Icon(Icons.add),
+                ),
               ),
               Text("3"),
             ],
