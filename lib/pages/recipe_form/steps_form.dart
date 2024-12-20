a4241f594fa865ea1b9bfa852af2c5da860f6d85
Hung Nguyen <hungnguyen@ankvn.com>
2024-12-20 16:07:28 +0700

    add listener to scroll controller

--- lib/pages/recipe_form/steps_form.dart
+++ lib/pages/recipe_form/steps_form.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class StepsForm extends StatefulWidget {
@@ -26,53 +27,108 @@ class CustomScrollableScrollPhysics extends ScrollPhysics {
 }
 
 class _StepsFormState extends State<StepsForm> {
-  late ScrollController _controller;
+  late ScrollController controller;
   double itemExtent = 0;
   double scrollHeight = 0;
   double scrollPadding = 20;
   double scrollbarPostition = 0;
+  double contentPostition = 0;
   double scrollbarHeight = 0;
   double itemListHeight = 0;
-  int itemsCount = 0;
   late Map<String, int> alphabetMap;
+  Map<String, double> contentOffsets = {};
+  Map<String, double> alphabetOffsets = {};
+  late final Iterable<String> alphabet;
+  double letterBlockPadding = 8.0;
+
+  double standardizedScrollbarHeight = 0;
+
+  double clampMaxScrollExtent(double value) {
+    return value.clamp(0, scrollHeight - scrollbarHeight);
+  }
 
   @override
   void initState() {
     super.initState();
-    _controller = ScrollController();
-    Iterable<int> alphabetList = Iterable.generate(26);
-    alphabetMap = {
-      for (var item in alphabetList) String.fromCharCode(item + 65): Random().nextInt(5)
-    };
-    alphabetMap['B'] = 27;
-    itemsCount = alphabetList.length;
+    controller = ScrollController();
+    alphabet = Iterable.generate(26, (index) => String.fromCharCode(index + 65));
+    alphabetMap = {for (String letter in alphabet) letter: (1 + Random().nextInt(5))};
+    alphabetMap['A'] = 2;
+    alphabetMap['B'] = 5;
+    alphabetMap['Z'] = 17;
+
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) {},
+    );
+    controller.addListener(
+      () {
+        var offsets = contentOffsets.values;
+        for (var i = 0; i < offsets.length; i++) {
+          // debugPrint("controller position");
+
+          // set scroll position = get (content offset - 1) when content offset > current content position
+          // ex: { A: 0, B: 100, C: 200 } and current content position = 150
+          // => identity C offset > current content position
+          // => current content position is between B and C => set scroll position is B
+          if (i > 0 && offsets.elementAt(i) > controller.position.pixels) {
+            setState(() {
+              // debugPrint("controller position ${controller.position.pixels}");
+              scrollbarPostition = alphabetOffsets.values.elementAt(i - 1);
+            });
+            break;
+          } else if (i == offsets.length - 1 && offsets.elementAt(i) < controller.position.pixels) {
+            // case scroll to 'Z', set scroll position when current content position > content offset 'Z'
+            setState(() {
+              // debugPrint("controller position ${controller.position.pixels}");
+              scrollbarPostition = alphabetOffsets.values.elementAt(i);
+            });
+            break;
+          }
+        }
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     if (itemExtent == 0) {
+      debugPrint("build itemExtent == 0");
       itemExtent = MediaQuery.sizeOf(context).height / 20;
-      itemListHeight = itemExtent * itemsCount;
+      itemListHeight = itemExtent * alphabet.length;
+
+      for (var i = 0; i < alphabetMap.length; i++) {
+        MapEntry<String, int> entry = alphabetMap.entries.elementAt(i);
+        if (i == 0) {
+          contentOffsets[entry.key] = 0;
+        } else {
+          // extent of a letter block =
+          //  letter block padding top        [letterBlockPadding]
+          //  + letter label                  [itemExtent]
+          //  + list of letter block          [itemExtent] * length
+          //  + letter block padding bottom   [letterBlockPadding]
+          contentOffsets[entry.key] = contentOffsets.values.last +
+              letterBlockPadding +
+              itemExtent +
+              itemExtent * alphabetMap.entries.elementAt(i - 1).value +
+              letterBlockPadding;
+        }
+      }
     }
     return Row(
       children: [
         Expanded(
-          child:
-              // Stack(
-              //   children: [
-              ScrollConfiguration(
-            behavior: ScrollConfiguration.of(context).copyWith(scrollbars: true),
+          child: ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
             child: ListView.builder(
               // itemExtent: itemExtent,
-              controller: _controller,
-              itemCount: itemsCount,
+              controller: controller,
+              itemCount: alphabet.length,
               itemBuilder: (BuildContext context, int index) {
                 MapEntry<String, int> entry = alphabetMap.entries.elementAt(index);
+
                 return Container(
-                  // color: index % 2 == 0 ? Colors.grey.shade300 : null,
                   padding: const EdgeInsets.all(8.0),
                   alignment: Alignment.centerLeft,
-                  // child: Text('Scrollable 1 : Index $index'),
                   child: Column(
                     children: [
                       Container(
@@ -82,7 +138,7 @@ class _StepsFormState extends State<StepsForm> {
                         alignment: Alignment.centerLeft,
                         child: Text(entry.key),
                       ),
-                      for (int i = 0; i <= entry.value; i++)
+                      for (int i = 1; i <= entry.value; i++)
                         Container(
                           color: Colors.blue.shade300,
                           height: itemExtent,
@@ -96,28 +152,12 @@ class _StepsFormState extends State<StepsForm> {
               },
             ),
           ),
-          // Positioned(
-          //   bottom: 10,
-          //   right: 10,
-          //   width: 100,
-          //   height: 30,
-          //   child: TextField(
-          //     onSubmitted: (value) {
-          //       // debugPrint(_controller.position.toString());
-          //       // debugPrint(_controller.positions.toString());
-          //       // debugPrint(_controller.position.pixels.toString());
-          //       // debugPrint(_controller.positions.length.toString());
-          //       _controller.jumpTo(itemExtent * int.parse(value));
-          //     },
-          //   ),
-          // ),
-          //   ],
-          // ),
         ),
         Builder(builder: (context) {
           WidgetsBinding.instance.addPostFrameCallback(
             (_) {
-              var renderbox = context.findRenderObject() as RenderBox;
+              RenderBox renderbox = context.findRenderObject() as RenderBox;
+              // debugPrint("WidgetsBinding.instance.addPostFrameCallback");
 
               /// set:
               /// - scrollbar's height
@@ -125,45 +165,54 @@ class _StepsFormState extends State<StepsForm> {
               if (scrollHeight == 0) {
                 setState(() {
                   scrollHeight = renderbox.size.height - scrollPadding * 2;
-                  scrollbarHeight = scrollHeight / 26;
+                  scrollbarHeight = scrollHeight / alphabetMap.length;
+                  standardizedScrollbarHeight = clampMaxScrollExtent(scrollbarHeight);
+                  alphabetOffsets = {
+                    for (int i = 0; i < alphabet.length; i++)
+                      alphabet.elementAt(i): scrollbarHeight * i
+                  };
                   // debugPrint("WidgetsBinding.instance.addPostFrameCallback");
                 });
               }
             },
           );
-
+          // debugPrint("Builder WidgetsBinding.instance.addPostFrameCallback");
           return Container(
             width: 15,
             margin: EdgeInsets.zero,
             padding: EdgeInsets.symmetric(vertical: scrollPadding),
             height: double.maxFinite,
             color: Colors.yellow.shade100,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.blueGrey.shade50,
-              ),
-              child: GestureDetector(
-                onVerticalDragUpdate: (details) {
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                // scroll in range [0..scroll height]
+                if (0 <= scrollbarPostition && scrollbarPostition <= scrollHeight) {
                   setState(() {
                     scrollbarPostition += details.delta.dy;
-                    debugPrint("onVerticalDragUpdate");
+                    debugPrint("onVerticalDragUpdate $scrollbarPostition ${details.localPosition}");
                   });
-                },
-                onPanDown: (details) {
-                  // set scrollbar position in range [0..scroll height - scrollbarHeight] to avoid overrflow
-                  setState(() {
-                    scrollbarPostition = details.localPosition.dy;
-                    debugPrint("onPanDown");
-                    // _controller.jumpTo(value)
-                  });
-                },
+                }
+              },
+              onPanDown: (details) {
+                // set scrollbar position in range [0..scroll height - scrollbarHeight] to avoid overrflow
+                setState(() {
+                  int index = (details.localPosition.dy / scrollbarHeight).floor();
+                  scrollbarPostition = alphabetOffsets.values.elementAt(index);
+                  controller.jumpTo(contentOffsets.values.elementAt(index));
+                  debugPrint("onPanDown ${details.localPosition.dy}");
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blueGrey.shade50,
+                ),
                 child: Stack(
                   children: [
                     AnimatedPositioned(
                       // set scrollbar position to center when drag
-                      top: (scrollbarPostition - scrollbarHeight / 2)
-                          .clamp(0, scrollHeight - scrollbarHeight),
+                      top: clampMaxScrollExtent(scrollbarPostition),
+                      // top: clampMaxScrollExtent(scrollbarPostition - scrollbarHeight / 2),
                       duration: Duration.zero,
                       child: Container(
                         width: 15,
@@ -176,15 +225,16 @@ class _StepsFormState extends State<StepsForm> {
                     ),
                     Column(
                       children: [
-                        for (int i = 65; i <= 90; i++)
+                        for (int i = 0; i < alphabet.length; i++)
                           Container(
-                            height: scrollbarHeight.clamp(0, scrollHeight - scrollbarHeight),
-                            decoration: i != 90
+                            height: standardizedScrollbarHeight,
+                            // color: i == alphabet.length - 1 ? Colors.amber : null,
+                            decoration: i < alphabet.length - 1
                                 ? const BoxDecoration(border: Border(bottom: BorderSide()))
                                 : null,
                             alignment: Alignment.center,
                             child: Text(
-                              String.fromCharCode(i),
+                              alphabet.elementAt(i),
                               style: Theme.of(context).textTheme.labelMedium,
                             ),
                           )
